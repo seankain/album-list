51d6deecff98064fc07d348d831ca4ed57181b33
Sean Kain <sean.kain.geo@gmail.com>
2026-04-22 22:19:33 -0400

    bug fixes to get something running finally

--- src/AlbumList/Views/AlbumListPage.xaml.cs
+++ src/AlbumList/Views/AlbumListPage.xaml.cs
@@ -5,8 +5,6 @@ namespace AlbumList.Views;
 
 public partial class AlbumListPage : ContentPage
 {
-    private Album? _menuAlbum;
-
     public AlbumListViewModel ViewModel => (AlbumListViewModel)BindingContext;
 
     public AlbumListPage(AlbumListViewModel vm)
@@ -15,24 +13,15 @@ public partial class AlbumListPage : ContentPage
         BindingContext = vm;
     }
 
-    private void OnMoreButtonClicked(object sender, EventArgs e)
-    {
-        if (sender is ImageButton button)
-        {
-            _menuAlbum = button.BindingContext as Album;
-            FlyoutBase.ShowAttachedFlyout(button);
-        }
-    }
-
     private void OnViewMenuItemClicked(object sender, EventArgs e)
     {
-        if (_menuAlbum is not null)
-            ViewModel.ViewCommand.Execute(_menuAlbum);
+        if (sender is MenuFlyoutItem { BindingContext: Album album })
+            ViewModel.ViewCommand.Execute(album);
     }
 
     private void OnDeleteMenuItemClicked(object sender, EventArgs e)
     {
-        if (_menuAlbum is not null)
-            ViewModel.DeleteCommand.Execute(_menuAlbum);
+        if (sender is MenuFlyoutItem { BindingContext: Album album })
+            ViewModel.DeleteCommand.Execute(album);
     }
 }
